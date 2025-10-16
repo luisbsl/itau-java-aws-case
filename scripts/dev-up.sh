bcb098da976483d9449e7667708965738ff60055
Luis Ismael Lima <luisbsl@gmail.com>
2025-10-15 21:52:28 -0300

    chore: remove obsolete dev-up script

--- scripts/dev-up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-docker compose up -d
-gradle wrapper --gradle-version 8.9
-chmod +x gradlew
-./gradlew :lambdas:payment-intake:shadowJar
-make package-lambda
-make apply
-./gradlew :services:payment-worker:bootRun
